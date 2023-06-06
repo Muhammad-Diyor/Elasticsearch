d837fd5dbd16588404ec12db8eb159eec72e31b7
Muhammad Diyor <muhammaddiyorimomnazarov@gmail.com>
2023-06-06 09:57:12 +0500

    ico

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -6,6 +6,7 @@ namespace ElasticSearchAPI.Controllers;
 [Route("[controller]")]
 public class WeatherForecastController : ControllerBase
 {
+    //
     private static readonly string[] Summaries = new[]
     {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
